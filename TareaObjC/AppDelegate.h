da43644f60f133a07e9e808feacc01174e19e439
g1sus <proyectos_technology@hotmail.com>
2015-01-17 19:50:48 -0600

    Initial Commit

---
+++ TareaObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TareaObjC
+//
+//  Created by Jesus Antonio Hernandez Mendez on 17/01/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TareaObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TareaObjC
+//
+//  Created by Jesus Antonio Hernandez Mendez on 17/01/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
